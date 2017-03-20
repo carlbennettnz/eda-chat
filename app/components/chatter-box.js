8b1c6b955536929a4cf31fc7fa9726132a00e139
Carl Bennett <carlbennettnz@gmail.com>
2017-03-21 12:56:27 +1300

    Add really basic chat ability

---
+++ app/components/chatter-box.js
@@ -0,0 +1,38 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  io: Ember.inject.service('socket-io'),
+
+  socket: null,
+  messages: [],
+  newMessage: '',
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    const socket = this.get('io').socketFor('ws://localhost:5000/');
+
+    socket.on('connect', () => {
+      console.log('opened');
+
+      socket.on('new-msg', message => this.addMessage(message));
+      socket.on('close', () => console.log('closed'));
+    });
+
+    this.set('socket', socket);
+  },
+
+  addMessage(message) {
+    this.get('messages').pushObject(message);
+  },
+
+  actions: {
+    send() {
+      console.log('sending');
+      this.get('socket').emit('new-msg', {
+        sender: 'Carl',
+        content: this.get('newMessage')
+      });
+    }
+  }
+});
